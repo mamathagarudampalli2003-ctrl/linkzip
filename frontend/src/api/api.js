0b287bad35e35e9243b04c5394ab0e2e3b336660
Mamatha Garudampalli <mamathagarudampalli2003@gmail.com>
2026-06-23 11:05:42 +0530

    Update API URL for production

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://localhost:8000/api";
+const API = `${import.meta.env.VITE_API_URL}/api`
 
 export const getUrls = (token) =>
   axios.get(`${API}/urls`, {
